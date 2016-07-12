788780f4bf02fd6ba10d13bf2fa2e5a30740dabe
HIVAladeen <lachiecs@hotmail.com>
2016-07-13 05:17:45 +0930

    lighting for levels basically done, unless someone has a problem with it. First orientation level room done(pick up objects), second orientaion room started(first enemy encounter).

---
+++ Avcon/Assets/lachieFolder/scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+	
+public class Rotator : MonoBehaviour {
+		
+		void Update ()
+		{
+			transform.Rotate (new Vector3 (25, 0, 0) * Time.deltaTime);
+		}
+	}
+
