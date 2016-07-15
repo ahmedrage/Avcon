58520c5af3385d3d45293b5515edf600bc301216
HIVAladeen <lachiecs@hotmail.com>
2016-07-16 02:20:36 +0930

    main menu

---
+++ Avcon/Assets/lachieFolder/scripts/menu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+
+	public void Play(){
+		StartCoroutine ("playDelay");
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+
+	IEnumerator playDelay(){
+		yield return new WaitForSeconds (0.25f);
+		SceneManager.LoadScene ("OneStart");
+	}
+}
