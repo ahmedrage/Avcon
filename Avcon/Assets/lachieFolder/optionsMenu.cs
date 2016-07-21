49d222ec41f9437b94a625663f19962771c74a43
HIVAladeen <lachiecs@hotmail.com>
2016-07-22 01:31:54 +0930

    options menu started, bugs fixed.

---
+++ Avcon/Assets/lachieFolder/optionsMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using XInputDotNetPure;
+using System.Collections;
+
+public class optionsMenu : MonoBehaviour{
+
+	public GameObject mainMenu;
+	public Toggle mute;
+
+	// Use this for initialization
+	void Start () 
+	{
+		mute.Select ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (GamePad.GetState (PlayerIndex.One).Buttons.B == ButtonState.Pressed) {
+			mainMenu.SetActive (true);
+			this.gameObject.SetActive (false);
+		}
+	}
+}
