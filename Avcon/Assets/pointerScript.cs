8c905801945f108580717a837cf5e239c5db76e1
ahmedrage <ahmedrage@hotmail.com>
2016-07-22 17:51:52 +0930

    Added task pointer

---
+++ Avcon/Assets/pointerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pointerScript : MonoBehaviour {
+	public GameObject target;
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Hands");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion lookRotation = Quaternion.LookRotation (target.transform.position - player.transform.position);
+		transform.rotation = lookRotation;
+		//transform.LookAt(target.transform.position);
+	}
+}
