74baa014c3de0e1eaa434aa6dd21440d300a4232
ahmedrage <ahmedrage@hotmail.com>
2016-07-25 22:10:17 +0930

    implemented Lachie's timer

--- Avcon/Assets/_Scripts/Timer.cs
+++ Avcon/Assets/_Scripts/Timer.cs
@@ -5,21 +5,26 @@ using System.Collections;
 public class Timer : MonoBehaviour {
 	public Text timerText;
 	private float startTime;
-	public float timer;
+	public float endTime;
 
+	[HideInInspector] bool completed;
 	// Use this for initialization
 	void Start () {
-		startTime = Time.time;
+		startTime = Time.time - endTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float t = Time.time - startTime; 
+		float t = -Time.time - startTime; 
 
-		string minutes = ((int)t / 60).ToString ();
-		string seconds = (t % 60).ToString("f0");
+		if (t > 0) {
+			string minutes = ((int)t / 60).ToString ();
+			string seconds = (t % 60).ToString ("f0");
 
-		timerText.text = "Time" + minutes + ":" + seconds;
-		//timer = minutes + seconds;
+			timerText.text = "Time " + minutes + ":" + seconds;
+		} else if (completed == false) {
+			timerText.text = "Time's up";
+			completed = true;
+		}
 	}
 }
